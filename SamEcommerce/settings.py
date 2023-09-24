c9249f2112080c6966a4748dc68c6e56761ef424
Samir Padekar <119828185+Samir1607@users.noreply.github.com>
2023-09-24 11:01:07 +0530

    Update settings.py
    updated settings.py

--- SamEcommerce/settings.py
+++ SamEcommerce/settings.py
@@ -129,7 +129,7 @@ MESSAGE_TAGS = { messages.ERROR: 'danger'}
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'samirpadekar99@gmail.com'
-EMAIL_HOST_PASSWORD ='dbcbyuwgovtlwyaj'
+EMAIL_HOST_PASSWORD ='ljgkjseh54th'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
